0f20c227bbb220e14a9f87f31b046dbace788286
William Huang <willhuang08@gmail.com>
2019-11-02 17:27:18 -0500

    Revert "Created party model (#4)" (#10)
    This reverts commit f4653c86374f1d14eaf4c299e7642f340145c1c0.

--- db/migrate/20181109025118_add_party_ref_to_submissions.rb
+++
@@ -1,6 +0,0 @@
-class AddPartyRefToSubmissions < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :submissions, :party, foreign_key: true
-    change_column :submissions, :party_id, :integer, null: false
-  end
-end
